407bb6d0ee999dd7894e4ca57f7224550fdc5c16
1129hiki <hikitaro2911@gmail.com>
2021-03-08 17:53:23 +0000

    session type to be redis

--- application.py
+++ application.py
@@ -32,11 +32,21 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-# Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
+app = Flask(__name__)
+# Check Configuration section for more details
+SESSION_TYPE = 'redis'
+app.config.from_object(__name__)
 Session(app)
+
+@app.route('/set/')
+def set():
+    session['key'] = 'value'
+    return 'ok'
+
+@app.route('/get/')
+def get():
+    return session.get('key', 'not set')
+
 
 @app.route('/', methods=["GET","POST"])
 @login_required
